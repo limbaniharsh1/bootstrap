a25f337102d9ee4049dd044f871ad05562a655bf
dhruvin2004 <dhruvinpandav999@gmail.com>
2023-04-05 18:52:25 +0530

    bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -8,33 +8,49 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 // import NavDropdown from 'NavDropdown';
 // import NavDropdown from 'react-bootstrap/NavDropdown';
-import { Person, Cart, Display } from 'react-bootstrap-icons';
+import { Person, Cart, Moon, Sun } from 'react-bootstrap-icons';
+
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { useState } from 'react';
 // import Badge from 'react-bootstrap/Badge'
 
 
 function App() {
+  let [state, setState] = useState(false)
+  let [color, setColor] = useState("dark")
+  console.log(state)
+
+
+  let handleClr = () => {
+    if (color == "dark") {
+
+      setColor("light")
+    }
+    else {
+      setColor('dark')
+    }
+  }
   return (
-    <div className="App">
-      <Navbar expand="lg" bg="light" variant="light">
-      <Container>
-        <Navbar.Brand href="#home"><img src='https://cdn.shopify.com/s/files/1/0027/0952/0502/files/logo_150x.png?v=1666114323'/></Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="m-auto"style={{alignItems:'center'}}>
-            {/* <Nav.Link href="#features" style={{color:'black'}}>Features</Nav.Link>
+    <div className="App"  style={{ transition: '0.5s'}}>
+      <Navbar expand="lg" bg={color} variant="light">
+        <Container>
+          <Navbar.Brand href="#home"><img src='https://cdn.shopify.com/s/files/1/0027/0952/0502/files/logo_150x.png?v=1666114323' /></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="m-auto" style={{ alignItems: 'center' }}>
+              {/* <Nav.Link href="#features" style={{color:'black'}}>Features</Nav.Link>
             <Nav.Link href="#pricing" style={{color:'black'}}>Pricing</Nav.Link> */}
-            <Form.Control
-              type="search"
-              placeholder="Search Our Store"
-              className="me-2"
-              aria-label="Search"
-              style={{width:'500px'}}
-            />
-            
-            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+              <Form.Control
+                type="search"
+                placeholder="Search Our Store"
+                className="me-2"
+                aria-label="Search"
+                style={{ width: '500px' }}
+              />
+
+              {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
               <NavDropdown.Item href="#action/3.2">
                 Another action
               </NavDropdown.Item>
@@ -44,15 +60,16 @@ function App() {
                 Separated link
               </NavDropdown.Item>
             </NavDropdown> */}
-          </Nav>
-          <Nav style={{alignItems:'center'}} className='d-flex'>
-          <Person color="dark" className='me-2' size='25px' />
-          <Cart color="dark" size='21px' />
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+            </Nav>
+            <Nav style={{ alignItems: 'center' }} className='d-flex'>
+              <Person color={color} className='me-2' size='25px' />
+              <Cart color="dark" size='21px' />
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
+      <div  bg={color} variant="light">
       <Container>
         <Row>
           <Col className='align-self-center' lg={6} sm={12}>
@@ -60,13 +77,15 @@ function App() {
             <p>Sit amet consectetur adipiscing Malesuada morbigrave ullamore alesuada nec pulvinar nisle</p>
             <button>order now</button>
           </Col>
-          <Col className='align-self-center' lg={6} sm={12}><img className='w-100' src='https://demo.webtend.net/html/qichen/assets/images/hero/hero-1.jpg' alt='img2'/></Col>
+          <Col className='align-self-center' lg={6} sm={12}><img className='w-100' src='https://demo.webtend.net/html/qichen/assets/images/hero/hero-1.jpg' alt='img2' /></Col>
         </Row>
       </Container>
+      </div>
 
-      <Container>
-        
-      </Container>
+      <div style={{ position: 'absolute', top: '50%', right: '50px' }}>
+      {color=="dark" ?  <Sun style={{ backgroundColor: 'cyan', height: '30px', width: '30px', padding: '6px', cursor: 'pointer' }} size='21px'  onClick={handleClr} /> : <Moon style={{ display: 'block', backgroundColor: 'cyan', height: '30px', width: '30px', padding: '6px', cursor: 'pointer' }} size='21px' className='mb-2'  onClick={handleClr} />}  
+      
+      </div>
 
     </div>
   );
